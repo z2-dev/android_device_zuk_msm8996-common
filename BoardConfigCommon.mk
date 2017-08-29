e67d288e7a00811622959a426e8463115e840ca5
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2017-08-29 23:04:10 +0200

    Remove local SDCLANG.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,13 +28,8 @@ VENDOR_PATH := device/zuk/msm8996-common
 
 TARGET_SPECIFIC_HEADER_PATH := $(VENDOR_PATH)/include
 
-
 # Use Snapdragon LLVM, if available
 TARGET_USE_SDCLANG := true
-LOCAL_SDCLANG := true
-SDCLANG := true
-SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8/bin
-SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msm8996
