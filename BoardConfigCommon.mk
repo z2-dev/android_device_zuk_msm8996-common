5c4fb50ca71d6f6c3b0ec73b566a76398ecec3ad
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2017-09-18 22:17:33 +0200

    Remove BOARD_USES_QC_TIME_SERVICES and thermal-engine blobs.
      * We use opensource Sony TimeKeep.
      * Keep vendor/lib/libthermalclient.so as it's a dependency of camera's blobs.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -201,9 +201,6 @@ BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 # Sensors
 USE_SENSOR_MULTI_HAL := true
 
-# Timeservice
-BOARD_USES_QC_TIME_SERVICES := true
-
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
